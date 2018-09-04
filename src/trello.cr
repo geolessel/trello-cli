e63d6012b163357f1947cb91fefcc78c58215830
Tim Morgan <tim@timmorgan.org>
2018-09-03 21:07:52 -0500

    Don't do curses things when running specs

--- src/trello.cr
+++ src/trello.cr
@@ -6,40 +6,44 @@ require "./*"
 module Trello
   VERSION = "0.1.0"
 
-  LibNCurses.setlocale(0, "") # enable unicode
-  NCurses.open do
-    NCurses.start_color
-    # NCurses.use_default_colors
-    NCurses.cbreak # CTRL-C breaks the program
-    NCurses.noecho # Don't print characters as the user types
-    NCurses.curs_set(0) # hide the cursor
-    NCurses.keypad(true) # allows arrow and F# keys
+  def self.start
+    LibNCurses.setlocale(0, "") # enable unicode
+    NCurses.open do
+      NCurses.start_color
+      # NCurses.use_default_colors
+      NCurses.cbreak # CTRL-C breaks the program
+      NCurses.noecho # Don't print characters as the user types
+      NCurses.curs_set(0) # hide the cursor
+      NCurses.keypad(true) # allows arrow and F# keys
 
-    boards = BoardsWindow.new
-    boards.activate!
+      boards = BoardsWindow.new
+      boards.activate!
 
-    lists = ListsWindow.new
-    lists.link_parent(boards)
+      lists = ListsWindow.new
+      lists.link_parent(boards)
 
-    cards = CardsWindow.new
-    cards.link_parent(lists)
+      cards = CardsWindow.new
+      cards.link_parent(lists)
 
-    App.windows = [boards, lists, cards] of Window
-
-    NCurses.refresh
-    App.windows.each { |w| w.refresh }
-
-    while true
-      NCurses.notimeout(true)
-      key = NCurses.getch
-      if key == NCurses::KeyCode::RESIZE
-        App.windows.each { |w| w.resize }
-      else
-        App.active_window.handle_key(key)
-      end
+      App.windows = [boards, lists, cards] of Window
 
       NCurses.refresh
-      App.windows.select { |w| w.visible }.each { |w| w.refresh }
+      App.windows.each { |w| w.refresh }
+
+      while true
+        NCurses.notimeout(true)
+        key = NCurses.getch
+        if key == NCurses::KeyCode::RESIZE
+          App.windows.each { |w| w.resize }
+        else
+          App.active_window.handle_key(key)
+        end
+
+        NCurses.refresh
+        App.windows.select { |w| w.visible }.each { |w| w.refresh }
+      end
     end
   end
 end
+
+Trello.start unless ENV.fetch("CRYSTAL_ENV", nil) == "test"

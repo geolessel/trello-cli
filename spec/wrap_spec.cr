242aeb8bc2a71510a80bdeae359def05b3f7f16f
Tim Morgan <tim@timmorgan.org>
2018-09-03 21:08:21 -0500

    Fix preservation of newlines when wrapping
    (and add some specs)

---
+++ spec/wrap_spec.cr
@@ -0,0 +1,27 @@
+require "./spec_helper"
+
+include Wrap
+
+describe Wrap do
+  it "wraps on spaces" do
+    result = wrap("Lorem ipsum dolor sit amet", 15)
+    result.should eq("Lorem ipsum\ndolor sit amet")
+  end
+
+  it "does not try to split a word that is correct width" do
+    result = wrap("Loremipsumdolor", 15)
+    result.should eq("Loremipsumdolor")
+    result = wrap("Loremipsumdolor sit amet", 15)
+    result.should eq("Loremipsumdolor\nsit amet")
+  end
+
+  it "will wrap a word that is too long without adding a blank line" do
+    result = wrap("Loremipsumdolor sit amet", 13)
+    result.should eq("Loremipsumdol\nor sit amet")
+  end
+
+  it "will preserve newlines" do
+    result = wrap("Lorem ipsum dolor\n\nsit amet", 13)
+    result.should eq("Lorem ipsum\ndolor\n\nsit amet")
+  end
+end
--- src/wrap.cr
+++ src/wrap.cr
@@ -4,10 +4,17 @@ module Wrap
     lines = [] of String
     until chars.empty?
       line = chars.shift(width + 1)
+      # preserve existing newlines
+      if (newline_index = line.index { |c| c == '\n' })
+        chars = line.last(line.size - newline_index - 1) + chars
+        line = line[0..newline_index]
+      end
+      # line has whitespace to break on, so be gentle
       if line.any?(&.whitespace?)
         until chars.empty? || line.size == 1 || line.last.whitespace?
           chars.unshift(line.pop)
         end
+      # line has no whitespace to break on, so be brutal
       elsif line.size > width
         chars.unshift(line.pop)
       end

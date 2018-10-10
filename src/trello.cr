71a4d7ea05652b4a484e20eb7eb53671473f3c3c
Geoffrey Lessel <geo@geoffreylessel.com>
2018-10-10 09:32:31 -0700

    Add command line flag to run through setup again

--- src/trello.cr
+++ src/trello.cr
@@ -49,6 +49,7 @@ unless ENV.fetch("CRYSTAL_ENV", nil) == "test"
 
   OptionParser.parse! do |parser|
     parser.banner = "trello\n\nUsage: trello [arguments]"
+    parser.on("--setup", "Before starting, run through the setup process again") { App.run_setup }
     parser.on("--setup-templates", "Set up editor templates") { App::Setup.make_templates; exit }
   end
 
